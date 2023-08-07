f8c7689d2ca3e597dc1f9f10dffd17e21e67f4af
Carlos Peraza <carlosperaza17@gmail.com>
2023-08-06 19:42:22 -0600

    Fix message

--- components/formwa.js
+++ components/formwa.js
@@ -37,7 +37,7 @@ const FormWA = () => {
 
     const onClickSend = () => {
         if(phoneNumber === ""){
-            setErrorMessage("plase enter a phone number");
+            setErrorMessage("please enter a phone number");
             return;
         }
         
